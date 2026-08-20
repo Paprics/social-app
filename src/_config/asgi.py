2954e1d6af174d6092c124c45ac32dc776039a4a
Valeriy D. <paprics@gmail.com>
2026-08-20 16:54:27 +0300

    Format ASGI configuration

--- src/_config/asgi.py
+++ src/_config/asgi.py
@@ -4,7 +4,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_config.settings")
 
 django_asgi_app = get_asgi_application()
@@ -12,15 +11,11 @@ django_asgi_app = get_asgi_application()
 import messenger.routing
 import video_chat.routing
 
-
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
-            URLRouter(
-                video_chat.routing.websocket_urlpatterns
-                + messenger.routing.websocket_urlpatterns
-            )
+            URLRouter(video_chat.routing.websocket_urlpatterns + messenger.routing.websocket_urlpatterns)
         ),
     }
 )
